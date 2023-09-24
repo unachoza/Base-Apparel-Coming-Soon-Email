179d4e1f5d0424e02057bd37b6368f04e2606a88
kabir-afk <kabirsingh12b@gmail.com>
2023-09-24 18:51:14 +0530

    added error icon on invalid email

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const submit = document.getElementById("submit-button");
 const form = document.querySelector("form");
 const errorMessage = document.querySelector(".error-message");
 const inputEmail = document.querySelector('input[type="email"]');
+const errorImg = document.querySelector(".error-image");
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -18,4 +19,5 @@ const validateForm = (e) => {
     ? "Your email has been submitted"
     : "Please provide a valid email";
   errorMessage.style.color = isValid ? "rgb(20, 209, 20)" : "#F96464";
+  errorImg.style.display = isValid ? "none" : "block";
 };
